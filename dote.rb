4cc6d8cdb2b6af2f3924c14a3a936052dd649052
atsu-chai <chai3milk@gmail.com>
2025-10-28 11:10:18 +0900

    version “1.0.0”

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/atsu-chai/homebrew-dote"
   url "https://raw.githubusercontent.com/atsu-chai/homebrew-dote/main/dote.c"
   sha256 "c54dfa7e60888cb27e9e00bc5dbd81e55c7f951b8bac5391edd3cd4543bad72a"
+  version “1.0.0”
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
